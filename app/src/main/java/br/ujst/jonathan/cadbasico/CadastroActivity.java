7b87358437483059fd49e44a0e47edee22e1999b
Jonathan Araujo <57550980+DjowNathan@users.noreply.github.com>
2020-10-09 00:00:37 -0300

    Initial commit

---
+++ app/src/main/java/br/ujst/jonathan/cadbasico/CadastroActivity.java
@@ -0,0 +1,14 @@
+package br.ujst.jonathan.cadbasico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
