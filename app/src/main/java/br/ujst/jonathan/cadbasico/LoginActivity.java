3837fba09ffa4c3583d4c64f5845fd4abd93d0f2
Jonathan Araujo <57550980+DjowNathan@users.noreply.github.com>
2020-10-09 17:25:21 -0300

    Restante da implementação

--- app/src/main/java/br/ujst/jonathan/cadbasico/LoginActivity.java
+++ app/src/main/java/br/ujst/jonathan/cadbasico/LoginActivity.java
@@ -2,16 +2,66 @@ package br.ujst.jonathan.cadbasico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.orhanobut.hawk.Hawk;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewNovoCadastro;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Hawk.init( this).build();
+
+        textViewNovoCadastro = findViewById(R.id.TextViewNovoCadastro);
+        buttonLogin = findViewById(R.id.ButtonLogin);
+
+        if(Hawk.contains("tem_cadastro")){
+
+            if(Hawk.get("tem_cadastro")){
+               habilitarLogin();
+            }else{
+                desabilitarLogin();
+            }
+        }else{
+           desabilitarLogin();
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(Hawk.contains("tem_cadastro")){
+            if(Hawk.get("tem_cadastro")){
+                habilitarLogin();
+            }else{
+                desabilitarLogin();
+            }
+        }else{
+            desabilitarLogin();
+        }
+    }
+
+    private void habilitarLogin(){
+        textViewNovoCadastro.setVisibility(View.GONE);
+        buttonLogin.setEnabled(false);
+        buttonLogin.setBackgroundColor(getColor(R.color.colorPrimary));
+    }
+
+    private void desabilitarLogin(){
+        textViewNovoCadastro.setVisibility(View.VISIBLE);
+        buttonLogin.setEnabled(false);
+        buttonLogin.setBackgroundColor(getColor(R.color.colorGray));
+
     }
 
     public void novoCadastro(View view) {
